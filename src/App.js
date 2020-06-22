58773da329b4549e1beb0d996df4cf5931edf7fb
quinqu <quinruby0@gmail.com>
2020-06-22 12:53:31 -0700

    router

--- src/App.js
+++ src/App.js
@@ -2,20 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import MovieSearch from './components/MovieSearch';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  
+    render () {
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/"> Home </Link>
+              </li>
+              <li>
+                <Link to="/search"> About </Link>
+              </li>
+              <li>
+                <Link to="/library"> Users </Link>
+              </li>
+            </ul>
+          </nav>
+  
+          <Switch>
+            <Route path="/search">
+              <MovieSearch url="" />
+            </Route>
+            <Route path="/library">
+              <Library />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    };
+  
   }
-}
 
 export default App;
